139b2441e690260b53b9bb7f89fe969ccb46c128
--unset <khushboo.goel@gmail.com>
2018-09-16 13:12:47 -0700

    fixed changes

--- whole_app_logic.js
+++ whole_app_logic.js
@@ -1,7 +1,7 @@
 
 var app = angular.module('myApp', []);
 app.controller('personCtrl', function($scope) {
-    $scope.firstName = "Nitin";
+    $scope.firstName = "Khushboo";
     $scope.lastName = "Goel";
     $scope.fullName = function() {
         return $scope.firstName + " " + $scope.lastName;
@@ -10,10 +10,12 @@ app.controller('personCtrl', function($scope) {
 
 app.controller('calc', function($scope) {
     $scope.res_So_Far = 0;
+    $scope.old_value = 0;
     $scope.operation = "";
     $scope.new_value = 0;
 
     $scope.calculate = function() {
+        $scope.old_value = $scope.res_So_Far;
         if($scope.operation == '+'){
             $scope.res_So_Far = $scope.res_So_Far+$scope.new_value;
         }
@@ -28,6 +30,9 @@ app.controller('calc', function($scope) {
         }
         if($scope.operation == '%'){
             $scope.res_So_Far = $scope.res_So_Far%$scope.new_value;
+        }
+        if($scope.operation == ""){
+            $scope.res_So_Far = $scope.new_value;
         }
         $scope.operation = "";
         $scope.new_value = 0;
